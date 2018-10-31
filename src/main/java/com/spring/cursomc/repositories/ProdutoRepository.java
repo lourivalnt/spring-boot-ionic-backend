3f99927c4bf8ce448f7ac672fc9342855030e451
Lourival Linard <l.linard@gmail.com>
2018-10-31 10:54:18 -0300

    Produto e assossiação muitos para muitos

---
+++ src/main/java/com/spring/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.spring.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
