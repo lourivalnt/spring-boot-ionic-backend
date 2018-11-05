864a1c510befac93b903ef6f10410b421bd13a54
Lourival Linard <l.linard@gmail.com>
2018-11-05 10:29:00 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/spring/cursomc/domain/Produto.java
+++ src/main/java/com/spring/cursomc/domain/Produto.java
@@ -32,9 +32,9 @@ public class Produto implements Serializable {
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
-//	@JsonIgnore
-//	@OneToMany(mappedBy = "id.produto")
-//	private Set<ItemPedido> itens = new HashSet<>();
+	@JsonIgnore
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 	}
@@ -46,14 +46,14 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
-//	@JsonIgnore
-//	public List<Pedido> getPedidos() {
-//		List<Pedido> lista = new ArrayList<>();
-//		for (ItemPedido x : itens) {
-//			lista.add(x.getPedido());
-//		}
-//		return lista;
-//	}
+	@JsonIgnore
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
 
 	public Integer getId() {
 		return id;
@@ -87,13 +87,13 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
-//	public Set<ItemPedido> getItens() {
-//		return itens;
-//	}
-//
-//	public void setItens(Set<ItemPedido> itens) {
-//		this.itens = itens;
-//	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
