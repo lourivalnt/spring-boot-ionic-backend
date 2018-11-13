2bd4a4c894ea974c167cd83bf8d5e880dad3e5ba
Lourival Linard <l.linard@gmail.com>
2018-11-13 09:10:14 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/spring/cursomc/resources/ClienteResource.java
+++ src/main/java/com/spring/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.spring.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.spring.cursomc.domain.Cidade;
 import com.spring.cursomc.domain.Cliente;
+import com.spring.cursomc.domain.Endereco;
+import com.spring.cursomc.domain.enums.TipoCliente;
 import com.spring.cursomc.dto.ClienteDTO;
+import com.spring.cursomc.dto.ClienteNewDTO;
 import com.spring.cursomc.services.ClienteService;
 
 @RestController
@@ -30,6 +36,16 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+//	@PreAuthorize("hasAnyRole('ADMIN')")
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 //	@PreAuthorize("hasAnyRole('ADMIN')")
