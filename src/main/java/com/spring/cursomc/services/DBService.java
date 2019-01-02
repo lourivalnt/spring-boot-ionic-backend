37010366d1484fadf6bca9c4eb317f901c738b7e
Lourival Linard <l.linard@gmail.com>
2019-01-02 08:30:52 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/spring/cursomc/services/DBService.java
+++ src/main/java/com/spring/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.spring.cursomc.domain.PagamentoComCartao;
 import com.spring.cursomc.domain.Pedido;
 import com.spring.cursomc.domain.Produto;
 import com.spring.cursomc.domain.enums.EstadoPagamento;
+import com.spring.cursomc.domain.enums.Perfil;
 import com.spring.cursomc.domain.enums.TipoCliente;
 import com.spring.cursomc.repositories.CategoriaRepository;
 import com.spring.cursomc.repositories.CidadeRepository;
@@ -33,7 +34,7 @@ import com.spring.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 	
@@ -115,18 +116,23 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "cursospring@bol.com.br", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
